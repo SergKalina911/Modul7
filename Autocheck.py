fa5b0edc38500c49617ef3e2a39dca2194afe1ad
Serg Kalynychenko <kalinasergey911@gmail.com>
2025-09-03 10:53:55 +0300

    Автопроверка 7

--- Autocheck.py
+++ Autocheck.py
@@ -604,4 +604,89 @@ print(d4)  # {1: 'a', 2: 'b'}
 """ Синтаксис прост, а код вполне понятен. Однако следует быть осторожным с переопределением математических 
 операторов, поскольку такое поведение обычно неочевидно и может сбивать с толку.
 
-"""
+7. Реализовать операции сложения и вычитания векторов для Vector класса. То есть переопределить математические 
+операторы __add__и __sub__для него.
+
+Имеется два вектора: a с координатами (x1, y1) и b с координатами (x2, y2).
+
+Тогда сложение векторов b + a создаёт новый вектор с координатами (x2 + x1, y2 + y1). Вычитание — обратная операция, 
+b - a даёт новый вектор с координатами (x2 - x1, y2 - y1).
+
+Пример:
+
+vector1 = Vector(Point(1, 10))
+vector2 = Vector(Point(10, 10))
+
+vector3 = vector2 + vector1
+vector4 = vector2 - vector1
+
+print(vector3)  # Vector(11,20)
+print(vector4)  # Vector(9,0)"""
+class Point:
+    def __init__(self, x, y):
+        self.__x = None
+        self.__y = None
+        self.x = x
+        self.y = y
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        if (type(x) == int) or (type(x) == float):
+            self.__x = x
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, y):
+        if (type(y) == int) or (type(y) == float):
+            self.__y = y
+
+    def __str__(self):
+        return f"Point({self.x},{self.y})"
+class Vector:
+    def __init__(self, coordinates: Point):
+        self.coordinates = coordinates
+
+    def __setitem__(self, index, value):
+        if index == 0:
+            self.coordinates.x = value
+        if index == 1:
+            self.coordinates.y = value
+
+    def __getitem__(self, index):
+        if index == 0:
+            return self.coordinates.x
+        if index == 1:
+            return self.coordinates.y
+
+    def __call__(self, value=None):
+        if value:
+            self.coordinates.x = self.coordinates.x * value
+            self.coordinates.y = self.coordinates.y * value
+        return self.coordinates.x, self.coordinates.y
+
+    def __add__(self, vector):
+        new_x = self.coordinates.x + vector.coordinates.x
+        new_y = self.coordinates.y + vector.coordinates.y
+        return Vector(Point(new_x, new_y))   
+        
+    def __sub__(self, vector):
+        new_x = self.coordinates.x - vector.coordinates.x
+        new_y = self.coordinates.y - vector.coordinates.y
+        return Vector(Point(new_x, new_y))        
+        
+    def __str__(self):
+        return f"Vector({self.coordinates.x},{self.coordinates.y})"
+
+vector1 = Vector(Point(1, 10))
+vector2 = Vector(Point(10, 10))
+vector3 = vector2 + vector1
+vector4 = vector2 - vector1
+print(vector3)  # Vector(11,20)
+print(vector4)  # Vector(9,0)
